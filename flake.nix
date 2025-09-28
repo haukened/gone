91de21cc67a5e2667703c02e31f1bf7b5cddb90d
David Haukeness <david@hauken.us>
2025-09-28 13:23:00 -0600

    Add initial flake configuration with inputs and outputs for development environments

---
+++ flake.nix
@@ -0,0 +1,69 @@
+{
+  description = "An empty flake template that you can adapt to your own environment";
+
+  # Flake inputs
+  inputs = {
+    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0"; # Stable Nixpkgs
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable"; # Unstable Nixpkgs
+  };
+
+  # Flake outputs
+  outputs =
+    { self, ... }@inputs:
+    let
+      # The systems supported for this flake's outputs
+      supportedSystems = [
+        "x86_64-linux" # 64-bit Intel/AMD Linux
+        "aarch64-linux" # 64-bit ARM Linux
+        "x86_64-darwin" # 64-bit Intel macOS
+        "aarch64-darwin" # 64-bit ARM macOS
+      ];
+
+      # Helper for providing system-specific attributes
+      forEachSupportedSystem =
+        f:
+        inputs.nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          f {
+            # Provides a system-specific, configured Nixpkgs
+            pkgs = import inputs.nixpkgs {
+              inherit system;
+              # Enable using unfree packages
+              config.allowUnfree = true;
+            };
+            # Also import unstable to cherry-pick newer packages like Go 1.25
+            unstable = import inputs.nixpkgs-unstable {
+              inherit system;
+              config.allowUnfree = true;
+            };
+          }
+        );
+    in
+    {
+      # Development environments output by this flake
+      devShells = forEachSupportedSystem (
+        { pkgs, unstable }:
+        {
+          # Run `nix develop` to activate this environment or `direnv allow` if you have direnv installed
+          default = pkgs.mkShell {
+            # The Nix packages provided in the environment
+            packages = [
+              unstable.go_1_25
+              unstable.delve
+            ] ++ (with pkgs; [
+              gosec
+              minify
+              go-task
+              git
+            ]);
+
+            # Set any environment variables for your development environment
+            env = { };
+
+            # Add any shell logic you want executed when the environment is activated
+            shellHook = "";
+          };
+        }
+      );
+    };
+}
